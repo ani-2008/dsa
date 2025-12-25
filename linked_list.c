fce8e1c91c46e75330515622cecb7077b643d9a5
bitabyte <anirudan2008@gmail.com>
2025-12-25 17:50:49 +0530

    insert into linked list and init linked list

--- linked_list.c
+++ linked_list.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// for simplicity the data of linked list will be a int
+// for simplicity the data of Node of linked list will be a int
 
 struct Node{
     int data;
     struct Node *next;
 
 };
+size_t sizeof_Node = sizeof(struct Node);
 
 void traverse(struct Node *head)
 {
@@ -19,23 +20,64 @@ void traverse(struct Node *head)
     printf("NULL\n");
 }
 
-void insert(struct Node *node, int data, int pos)
+void free_ll(struct Node *head)
+{
+    struct Node *node = head;
+    while(node != NULL){
+        struct Node *temp = node;
+        node = node->next;
+        free(temp);
+    }
+}
+
+void insert(struct Node *node, int data)
 {
     
+    if (node != NULL){
+        struct Node *new = malloc(sizeof_Node);
+        if (new == NULL){
+            printf("Can't insert\n");
+        }else{
+            new->data = data;
+            new->next = node->next;
+        
+            node->next = new;
+        }
+        
+    }else{printf("Can't insert after a NULL node\n");}
+    
+}
+
+struct Node *ll_init(int data)
+{
+    struct Node *head = malloc(sizeof_Node);
+    if (head != NULL){
+
+        head->data = data;
+        head->next = NULL;
+        return head;
+    }else{printf("Can't insert\n");}
 }
 
 int main()
 {
-    struct Node *head = malloc(sizeof(struct Node));
-    struct Node *first = malloc(sizeof(struct Node));
-    head->data = 0;
-    head->next = first;
+    
+    struct Node *first = malloc(sizeof_Node);
     first->data = 1;
     first->next = NULL;
+    struct Node *head = ll_init(0);
+    head->next = first;
 
-    traverse(head);
 
-    free(head);
-    free(first);
+    traverse(head); // 0->1->NULL
+    
+    insert(first,2); 
+    
+    traverse(head); // 0->1->2->NULL
+    
+    insert(first,1);
 
+    traverse(head); // 0->1->1->2->NULL
+
+    free_ll(head); 
 }
