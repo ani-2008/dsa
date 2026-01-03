45c29af9f43131cf0b4cd5571023fbf3eef57cc4
bitabyte <anirudan200@gmail.com>
2026-01-03 20:43:57 +0530

    dynamic arrays

---
+++ src/dynamic_arrays.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int *items;
+    size_t count;
+    size_t size;
+} DynamicArray;
+
+void append(DynamicArray *array, int item)
+{
+    do{
+        if(array->count >= array->size){
+            if(array->size == 0){
+                array->size = 100;
+            }else{array->size *= 2;}
+            array->items = realloc(array->items,array->size*(sizeof(array->size)));
+        }
+        array->items[array->count++] += item;
+    }while(0);
+
+}
+
+int main()
+{
+    DynamicArray array = {0};
+
+    for(int i = 0; i < 10; i++){
+        append(&array,i);
+    }
+    
+    for(size_t i = 0; i < 10; i++){
+        printf("%d ",array.items[i]);
+    }
+    printf("\n");
+}
